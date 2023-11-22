852c55d3d51ccb691f142c0a8e4d62eba9c33eeb
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2023-11-22 22:59:54 +0100

    fix mongoose

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 
 const connectDb = async() => {
     try{
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        const connection = await mongoose.connect(process.env.CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log(
          "Database connected: ",
          connect.connection.host,
