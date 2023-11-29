533c2d2d7a36de5a2e6c5a1077181f3be816830c
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2023-11-29 11:36:12 +0100

    try to fix refresh

--- services/tokenService.js
+++ services/tokenService.js
@@ -10,7 +10,7 @@ function generateTokens (payload) {
 
  function validateAccessToken(token){
 	try {
-		console.log("The token is :",token);
+		console.log("The accessToken is :",token);
 		const userData = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 		console.log(userData);
 		return userData;
@@ -21,7 +21,7 @@ function generateTokens (payload) {
 }
  function validateRefreshToken(token){
 	try {
-		console.log("The token is :",token);
+		console.log("The refreshToken is :",token);
 		const userData = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
 		return userData;
 	} catch (error) {
