5580f5bd8c32057d9bfe727c1b076967927fee78
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2024-01-02 15:30:33 +0100

    fix errors without html page only json

--- middleware/apiError.js
+++ middleware/apiError.js
@@ -1,3 +1,4 @@
+
 module.exports = class ApiError extends Error {
 	status;
 	errors;
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,9 @@
 const ApiError = require("./apiError");
 
-module.exports = function (err, req, res, next,message) {
+module.exports = function (err, req, res, next) {
+
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message, errors: err.errors });
   }
-  return res.status(500).json({ message: err.message, errors: err.errors });
-}
+  return res.status(500).json({ message: "Unexpected error" });
+};
