7e3c2028fc0870f82c7a0a8ffc9266bab441d701
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2024-01-09 11:54:16 +0100

    f35

--- services/googleOauthService.js
+++ services/googleOauthService.js
@@ -63,7 +63,7 @@ const getGoogleOauthTokens = asyncHandler(async (code) => {
 		{ email: googleUserData.email },
 		{ username: googleUserData.name,
 		  picture: googleUserData.picture,
-		  password: bcrypt.hashSync(null, 10),
+		  password: null,
 		  changePasswordLink: null,
 		  isActivated: true},
 		  
